558aa4eca12a3cdd512a4f652d3e973677407b9e
Argha Bhowmick <arghabhowmick003@gmail.com>
2024-07-18 20:35:51 +0530

    Overall changes

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const currentTime = () => {
+  let curTime = new Date().toLocaleTimeString();
+  document.getElementById("clock").innerText = curTime;
+};
+currentTime();
+const intervalId = setInterval(() => {
+  currentTime();
+}, 1000);
 
+setTimeout(() => clearInterval(intervalId), 5000);
